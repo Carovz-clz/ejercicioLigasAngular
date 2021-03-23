6be7f7f4519819c6f3f607e971a1e62eb701d764
Carolina Zapata <cvzapata@minsait.com>
2021-03-23 09:37:48 +0100

    Rutas añadidas al menú

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginModalComponent } from './login-modal/login-modal.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
-  {path:'', component: LoginModalComponent, pathMatch: 'full'}
+  {path:'', redirectTo: '/inicio', pathMatch: 'full'},
+  {path:'inicio', component: InicioComponent},
+  {path:'eventos', component: EventosComponent},
 ];
 
 @NgModule({
