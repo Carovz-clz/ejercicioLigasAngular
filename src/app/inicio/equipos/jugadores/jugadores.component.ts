037cef580efd4b521f70649079b349c109614bd9
Carolina Zapata <cvzapata@minsait.com>
2021-03-25 15:28:51 +0100

    Botones añadidos al componente jugadores. Volver funciona

--- src/app/inicio/equipos/jugadores/jugadores.component.ts
+++ src/app/inicio/equipos/jugadores/jugadores.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Jugador } from 'src/app/shared/jugador.model';
 import { ObtencionDatosService } from 'src/app/shared/obtencion-datos.service';
 
@@ -13,12 +13,23 @@ export class JugadoresComponent implements OnInit {
   arrayJugadores: Jugador[];
   idEquipo: number;
 
-  constructor(private ruta: ActivatedRoute, private obDatosService: ObtencionDatosService) { }
+  constructor(private ruta: ActivatedRoute, private router: Router, private obDatosService: ObtencionDatosService) { }
 
   ngOnInit(): void {
     this.idEquipo = this.ruta.snapshot.params['idequipo'];
     this.arrayJugadores = this.obDatosService.obtenerJugadores(this.idEquipo);
     console.log(this.arrayJugadores);
+
+    this.ruta.params.subscribe(
+      (params: Params) => {
+        this.idEquipo = params['idequipo'];
+        this.arrayJugadores = this.obDatosService.obtenerJugadores(this.idEquipo);
+      }
+    );
+  }
+
+  volver(){
+    this.router.navigate(['../'], {relativeTo: this.ruta});
   }
 
 }
